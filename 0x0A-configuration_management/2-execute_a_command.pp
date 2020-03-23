26a8533477e0c8688e610819bd307d9ce6d0e1a7
Virteip <sergiopietri@gmail.com>
2020-03-23 16:36:04 -0500

    [Fixed betty]

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #Grant a program's dying wish
 exec { '2-execute_a_command':
-  path     =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  command  =>  'pkill -f killmenow',
-  returns  =>  [0,1,]}
+  path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', ],
+  command =>  'pkill -f killmenow',
+  returns =>  [0,1,]}
