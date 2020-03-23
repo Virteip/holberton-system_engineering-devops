30ffa22a2bd0e39ac64fe555972d870e73a1e14a
Virteip <sergiopietri@gmail.com>
2020-03-23 13:36:55 -0500

    [Add Contents Initialize folder]

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+file { '0-create_a_file.pp':
+  ensure  =>  present,
+  path    =>  '/tmp/holberton',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet'}
