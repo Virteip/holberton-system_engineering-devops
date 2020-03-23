8b9affec2aabdf7adaa328f1eefadfefd1ab0604
Virteip <sergiopietri@gmail.com>
2020-03-23 16:26:05 -0500

    [Added commentar]

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,2 +1,5 @@
-exec { '2-execute_a_command.pp':
-  command  =>  'pkill -f killmenow'}
+#Grant a program's dying wish
+exec { '2-execute_a_command':
+  path     =>  ['usr/bin', '/usr/sbin',],
+  provider => shell,
+  command  =>  'pkill -f killmenow',}
