74bef01e7a4d31c529d21ec02c03eb8b5b6d6fc4
Virteip <sergiopietri@gmail.com>
2020-03-24 09:38:27 -0500

    [T#4 (0)]

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+define resolve() {
+    $str = "
+    	 ForwardAgent yes
+    	 IdentityFile ~/.ssh/holberton
+    	 PasswordAuthentication no
+        "
+
+    file { "/etc/ssh/ssh_config":
+      content => "$str",
+    }
+}
