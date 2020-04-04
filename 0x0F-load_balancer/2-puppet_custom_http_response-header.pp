8906be0bf28b58f4e95f9e00902997e9dd32c856
Virteip <sergiopietri@gmail.com>
2020-04-03 23:03:33 -0500

    [Header]

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,7 @@
+# Http header
+exec { 'Http_Header':
+  command  => 'sudo sed -i\
+  "33i\      add_header X-Served-By \$HOSTNAME;"\
+  /etc/nginx/sites-available/default; sudo service haproxy restart',
+  provider => shell,
+}
