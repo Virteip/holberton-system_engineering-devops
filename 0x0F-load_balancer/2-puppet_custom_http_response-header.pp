cd36eec86891305b44ae3c9237b15fd728269c08
Virteip <sergiopietri@gmail.com>
2020-04-03 23:16:56 -0500

    [Header]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,7 @@
-# Http header
+#Http header
 
 exec { 'Custom_HttpHeader':
-  command  => 'sudo apt-get update; sudo apt-get install nginx -y; sudo sed -i "36i\ add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-enabled/default; sudo service haproxy start',
+  command  => 'sudo apt-get update; sudo apt-get install nginx -y; sudo sed -i "36i\ add_header X-Served-By \$HOSTNAME;"\
+  /etc/nginx/sites-enabled/default; sudo service haproxy start',
   provider => shell,
 }
