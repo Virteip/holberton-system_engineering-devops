a0cd2b8655980fa9cb1e154d6b4c21b55bb4c58b
Virteip <sergiopietri@gmail.com>
2020-05-04 15:02:49 -0500

    [Add]

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Fetch data from an API"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id=" + argv[1])
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
+
+    users_json = users.json()
+    todos_json = todos.json()
+    task_list = []
+    employee_name = users_json[0]['name']
+
+    with open('USER_ID.csv', 'w') as f:
+        write = csv.writer(f)
+        for data in todos_json:
+            write.writerow([argv[1], employee_name,
+                           data['completed'], data['title']])
