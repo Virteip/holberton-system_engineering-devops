a629ce2d140684f2f4b0e9498da7ad24003f6a20
Virteip <sergiopietri@gmail.com>
2020-05-07 13:57:37 -0500

    [Fix] re-adding try

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,15 +7,18 @@ from sys import argv
 
 def recurse(subreddit, hot_list=[], x=0):
     """ All the hotness """
-    header = {'user-agent': 'sergio/0.0.1'}
-    fetch = requests.get(
-        "https://www.reddit.com/r/" + argv[1] + "/hot.json",
-        headers=header, allow_redirects=False)
-    data_j = fetch.json()
+    try:
+        header = {'user-agent': 'sergio/0.0.1'}
+        fetch = requests.get(
+            "https://www.reddit.com/r/" + argv[1] + "/hot.json",
+            headers=header, allow_redirects=False)
+        data_j = fetch.json()
 
-    title = data_j.get('data').get('children')[x].get('data')['title']
+        title = data_j.get('data').get('children')[x].get('data')['title']
 
-    if title is not None:
-        hot_list.append(title)
-        recurse(subreddit, hot_list, x+1)
-    return hot_list
+        if title is not None:
+            hot_list.append(title)
+            recurse(subreddit, hot_list, x+1)
+        return hot_list
+    except:
+        pass
