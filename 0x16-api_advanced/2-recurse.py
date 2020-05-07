64aea6963ff7fed120b7985bb632ca6251649fc7
Virteip <sergiopietri@gmail.com>
2020-05-07 14:52:10 -0500

    idk

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,7 +18,7 @@ def recurse(subreddit, hot_list=[], x=0):
 
         if title is not None:
             hot_list.append(title)
-            recurse(argv[1], hot_list, x+1)
+            recurse(subreddit, hot_list, x+1)
         return hot_list
     except:
         return None
