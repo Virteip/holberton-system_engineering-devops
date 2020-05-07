eec865ee9395f9f309391369c797aca05c8adfc4
Virteip <sergiopietri@gmail.com>
2020-05-07 14:40:48 -0500

    [Fix] re-adding try

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,7 +10,7 @@ def recurse(subreddit, hot_list=[], x=0):
     try:
         header = {'user-agent': 'sergio/0.0.1'}
         fetch = requests.get(
-            "https://www.reddit.com/r/" + argv[1] + "/hot.json",
+            "https://www.reddit.com/r/" + subreddit + "/hot.json",
             headers=header, allow_redirects=False)
         data_j = fetch.json()
 
@@ -19,6 +19,7 @@ def recurse(subreddit, hot_list=[], x=0):
         if title is not None:
             hot_list.append(title)
             recurse(subreddit, hot_list, x+1)
+        print(hot_list)
         return hot_list
     except:
-        pass
+        return None
