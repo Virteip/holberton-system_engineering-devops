37c45db25ef1fbe8c9c0b8857924c2fbd18d1867
Virteip <sergiopietri@gmail.com>
2020-05-29 08:02:20 +0000

    [Fix] Lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # Web Stack debug
-exec { 'task0':
-  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 15000\"/" /etc/default/nginx; sudo service nginx restart',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0, 1]
+exec { 'task0 increase limit':
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 15000\"/" /etc/default/nginx; sudo service nginx restart',
+  provider => 'shell',
+  path     => ['/usr/bin', '/bin'],
+  returns  => [0, 1]
 }
